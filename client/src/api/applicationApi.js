58da097475dd0ea293f425a4da0c4b92815d92dc
prevyne <prevyneoketch6@gmail.com>
2025-07-18 15:43:59 +0300

    fix: Finalize axios config to send credentials

--- client/src/api/applicationApi.js
+++ client/src/api/applicationApi.js
@@ -1,16 +1,13 @@
 import api from './axiosConfig';
 
 export const applyForJob = async (jobId) => {
-  // No token needed here, cookie is sent automatically
   const response = await api.post(`/applications/${jobId}`, null);
   return response.data;
 };
-
 export const getMyApplications = async () => {
   const response = await api.get('/applications/seeker/me');
   return response.data;
 };
-
 export const getApplicantsForJob = async (jobId) => {
   const response = await api.get(`/applications/job/${jobId}`);
   return response.data;
--- client/src/api/jobApi.js
+++ client/src/api/jobApi.js
@@ -5,17 +5,14 @@ export const getAllJobs = async (filters = {}) => {
   const response = await api.get(`/jobs?${params}`);
   return response.data;
 };
-
 export const getJobById = async (jobId) => {
   const response = await api.get(`/jobs/${jobId}`);
   return response.data;
 };
-
 export const getMyJobs = async () => {
   const response = await api.get('/jobs/myjobs');
   return response.data;
 };
-
 export const createJob = async (jobData) => {
   const response = await api.post('/jobs', jobData);
   return response.data;
